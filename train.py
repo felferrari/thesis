133050159aae28d949a4a6a81bc3956da079eb24
felferrari <felferrari@gmail.com>
2023-07-27 12:13:16 -0300

    m

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import argparse
 from utils.ops import count_parameters, save_yaml, load_yaml
 import torch
 from torch.multiprocessing import Process, freeze_support
-from torch.utils.data import DataLoader, RandomSampler
+from torch.utils.data import DataLoader
 import time
 from  pathlib import Path
 from utils.datasets import TrainDataset, ValDataset#, to_gpu
@@ -11,6 +11,7 @@ import lightning.pytorch as pl
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers.tensorboard import TensorBoardLogger
+from lightning.pytorch.profilers import SimpleProfiler
 #from fvcore.nn import flop_count_table, FlopCountAnalysis, flop_count
 
 parser = argparse.ArgumentParser(
@@ -27,7 +28,7 @@ parser.add_argument( # The path to the config file (.yaml)
 parser.add_argument( # Experiment number
     '-e', '--experiment',
     type = int,
-    default = 1,
+    default = 99,
     help = 'The number of the experiment'
 )
 
@@ -62,7 +63,7 @@ parser.add_argument( # Log in neptune
 parser.add_argument( # specific site location number
     '-s', '--site',
     type = int,
-    default=1,
+    default=2,
     help = 'Site location number'
 )
 
@@ -231,21 +232,29 @@ def run(model_idx):
         #flops = flop_count(model, inputs)
 
         early_stop_callback = EarlyStopping(monitor="val_loss", verbose = True, mode="min", **training_params['early_stop'])
+
         monitor_checkpoint_callback = ModelCheckpoint(
             str(models_path), 
             monitor = 'val_loss', 
             verbose = True, 
             filename = f'model_{model_idx}'
             )
+        
+        profiler = SimpleProfiler(
+            dirpath = logs_path,
+            filename = f'model_{model_idx}',
+            )
+
         trainer = pl.Trainer(
             accelerator  = 'gpu',
             devices = args.devices,
             limit_train_batches = training_params['max_train_batches'], 
             limit_val_batches = training_params['max_val_batches'], 
-            max_epochs = training_params['max_epochs'], 
+            max_epochs = 1, #training_params['max_epochs'], 
             callbacks = [early_stop_callback, monitor_checkpoint_callback], 
             logger = loggers,
             log_every_n_steps = 1,
+            profiler =profiler,
             #num_sanity_val_steps = 0
             enable_progress_bar = False
             )
