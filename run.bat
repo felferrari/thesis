32288ea38b92e6bc75d6e3a9bc9949b216ff4018
Felipe Ferrari <felferrari@gmail.com>
2023-08-04 10:59:01 -0300

    m

--- run.bat
+++ run.bat
@@ -1,17 +1,9 @@
-echo python tiles-gen.py
-echo python previous-def-gen.py
-echo python label-gen.py
-echo python cloud-map-gen.py
-
-echo python prepare-data.py --statistics
-echo python prepare-data.py --train-data
+python previous-def-gen.py -s 1
+python label-gen.py -s 1
+python tiles-gen.py -s 1
+python prepare-data.py -s 1 -r
 
 
-python train.py -s 1 -e 12
-python predict.py -s 1 -e 12
-python evaluate.py -s 1 -e 12
-
-python train.py -s 1 -e 11
-python predict.py -s 1 -e 11
-python evaluate.py -s 1 -e 11
-
+python train.py -s 1 -e 6
+python predict.py -s 1 -e 6
+python evaluate.py -s 1 -e 6
