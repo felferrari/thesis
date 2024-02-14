9e2207dd5d6daff00449163ce6f63a7327b8da1c
Felipe Ferrari <felferrari@gmail.com>
2024-02-14 12:34:50 -0300

    m

--- run.bat
+++ run.bat
@@ -1,14 +1,87 @@
-python predict.py -s 1 -e 55
-python evaluate.py -s 1 -e 55
+python train.py -s 2 -e 1
+python predict.py -s 2 -e 1
+python evaluate.py -s 2 -e 1
 
-python predict.py -s 1 -e 56
-python evaluate.py -s 1 -e 56
 
-python predict.py -s 1 -e 57
-python evaluate.py -s 1 -e 57
+python train.py -s 2 -e 2
+python predict.py -s 2 -e 2
+python evaluate.py -s 2 -e 2
 
-python predict.py -s 1 -e 58
-python evaluate.py -s 1 -e 58
+python train.py -s 2 -e 3
+python predict.py -s 2 -e 3
+python evaluate.py -s 2 -e 3
 
-python predict.py -s 1 -e 59
-python evaluate.py -s 1 -e 59
+python train.py -s 2 -e 4
+python predict.py -s 2 -e 4
+python evaluate.py -s 2 -e 4
+
+python predict.py -s 2 -e 3
+python evaluate.py -s 2 -e 3
+
+python train.py -s 2 -e 5
+python predict.py -s 2 -e 5
+python evaluate.py -s 2 -e 5
+
+python train.py -s 2 -e 6
+python predict.py -s 2 -e 6
+python evaluate.py -s 2 -e 6
+
+python train.py -s 2 -e 7
+python predict.py -s 2 -e 7
+python evaluate.py -s 2 -e 7
+
+python train.py -s 2 -e 8
+python predict.py -s 2 -e 8
+python evaluate.py -s 2 -e 8
+
+python train.py -s 2 -e 9
+python predict.py -s 2 -e 9
+python evaluate.py -s 2 -e 9
+
+python train.py -s 2 -e 10
+python predict.py -s 2 -e 10
+python evaluate.py -s 2 -e 10
+
+python train.py -s 2 -e 11
+python predict.py -s 2 -e 11
+python evaluate.py -s 2 -e 11
+
+python train.py -s 2 -e 12
+python predict.py -s 2 -e 12
+python evaluate.py -s 2 -e 12
+
+python train.py -s 2 -e 13
+python predict.py -s 2 -e 13
+python evaluate.py -s 2 -e 13
+
+python train.py -s 2 -e 14
+python predict.py -s 2 -e 14
+python evaluate.py -s 2 -e 14
+
+python train.py -s 2 -e 15
+python predict.py -s 2 -e 15
+python evaluate.py -s 2 -e 15
+
+python train.py -s 2 -e 16
+python predict.py -s 2 -e 16
+python evaluate.py -s 2 -e 16
+
+python train.py -s 2 -e 17
+python predict.py -s 2 -e 17
+python evaluate.py -s 2 -e 17
+
+python train.py -s 2 -e 18
+python predict.py -s 2 -e 18
+python evaluate.py -s 2 -e 18
+
+python train.py -s 2 -e 19
+python predict.py -s 2 -e 19
+python evaluate.py -s 2 -e 19
+
+python train.py -s 2 -e 20
+python predict.py -s 2 -e 20
+python evaluate.py -s 2 -e 20
+
+python train.py -s 2 -e 21
+python predict.py -s 2 -e 21
+python evaluate.py -s 2 -e 21
